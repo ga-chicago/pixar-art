f428788de4b1273a89b96c5bd9e33bd13f6c43e1
Zoe <zoereagan@gmail.com>
2018-03-23 15:24:17 -0500

    set color button and change color of brush

--- pixart.js
+++ pixart.js
@@ -1,1 +1,31 @@
+console.log("hello");
+let $button = $('#set-color')
+let $brushBox = $('.brush')
+let $input = $('input')
 
+$button.on('click', (e) => {
+  $brushBox.css('background-color', $input.val())
+  console.log("there was a click");
+  event.preventDefault()
+})
+
+// // When I click the "Set Color" button
+// //declaring a variable for set color button
+// const $setColor = $('#set-color')
+//
+// $setColor.on('click', (e) => {
+// //fuction here need to change the color of "brush box"
+// //to the color specified
+//
+//
+//
+// getColor(){
+//     const color = $("#color-field").input.val()
+//     return ??
+//   }
+// getColor();
+//
+//  setBrush() {
+//    const brush = $('.brush')
+//  }
+// setBrush();
