244a49839a892a745b37e239aee670bf48c22ceb
marietrull <marietrull@gmail.com>
2018-03-23 15:00:16 -0500

    Commit 2: Completed Step Three

--- pixart.js
+++ pixart.js
@@ -2,8 +2,30 @@
 let $button = $('#set-color');
 let brushBox = $('.brush');
 let input = $('input');
+
+//Commit 1 & 2
 $button.on('click', (event) => {
 	brushBox.css('background-color', input.val())
 	console.log('there was a click');
 	event.preventDefault();
 })
+
+//Commit 3
+//Create 20 divs of the "square" class and append them to the body
+
+const addSquare = () => {
+
+	for(let i = 0; i < 21; i++){
+
+	// Create div element of the square class
+	const $div = $('<div>');
+	$div.addClass('square');
+
+	// Append to the body
+	$div.appendTo($('body'));
+	
+	}
+	
+}
+
+addSquare();
