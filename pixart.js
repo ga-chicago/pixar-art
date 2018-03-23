28460d913259eedce8d0439959fdd867a1ef5166
agraham <agraham0925@gmail.com>
2018-03-23 14:52:08 -0500

    set color button complete - woohoo

--- pixart.js
+++ pixart.js
@@ -1,1 +1,23 @@
+// When I click the "Set Color" button, it should change the color of the "brush" box to the color I specify in the input field.
+// Use jQuery to select the element and add an event listener
+  
+ //locate div for brush box
+    // <form id="form">
+    //  <input type="text" id="color-field"></input>
+    //  <button id="set-color">Set Color</button>
+    // </form>
+    // <div class="brush"></div>
+
+let brushBox = $('.brush');
+let button = $('#set-color')
+let input = $('input')
+ //change color of box
+button.on('click', function (event) {
+	brushBox.css('background-color', input.val())
+	console.log('there was a click')
+	event.preventDefault();
+})
+
+//create something to connect the input value to rgb value property
+
 
