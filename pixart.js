7f29a56530eee504f02d5eb93d30f09e69112565
Zoe <zoereagan@gmail.com>
2018-03-23 15:29:54 -0500

    commit 3

--- pixart.js
+++ pixart.js
@@ -1,31 +1,28 @@
 console.log("hello");
+//declaring a variable for set color button
 let $button = $('#set-color')
 let $brushBox = $('.brush')
 let $input = $('input')
-
+//commit 1 & 2
+// When I click the "Set Color" button
+//it should change the color of the "brush" box to the color
+//I specify in the input field.
 $button.on('click', (e) => {
   $brushBox.css('background-color', $input.val())
   console.log("there was a click");
   event.preventDefault()
 })
 
-// // When I click the "Set Color" button
-// //declaring a variable for set color button
-// const $setColor = $('#set-color')
-//
-// $setColor.on('click', (e) => {
-// //fuction here need to change the color of "brush box"
-// //to the color specified
-//
-//
-//
-// getColor(){
-//     const color = $("#color-field").input.val()
-//     return ??
-//   }
-// getColor();
-//
-//  setBrush() {
-//    const brush = $('.brush')
-//  }
-// setBrush();
+//commit 3
+//Create 20 divs of the "square" class
+//and append them to the body
+
+
+const createSquare = () => {
+  for (let i = 0;  i < 20; i++) {
+    const $div = $('<div>')
+    $div.addClass('square')
+    $div.appendTo($('body'))
+  }
+}
+createSquare();
