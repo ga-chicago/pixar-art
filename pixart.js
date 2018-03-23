eaad374e15d81cfd8456fc846672a41ee765a67d
BenCharbonneau <benscharb@gmail.com>
2018-03-23 16:11:07 -0500

    Added click listener to change brush color (commit 1 and 2)

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
+function setColor(event) {
+	event.preventDefault();
+	let color = $('#color-field').val();
+	if(color) $('.brush').css('background-color',color);
+}
+
+$('#set-color').on('click', setColor);
+
 
