6459f631df0a7ebad6c72601d2e5424f0a25b804
marietrull <marietrull@gmail.com>
2018-03-23 14:51:17 -0500

    Commit 1: Completed Step One

--- pixart.js
+++ pixart.js
@@ -1,1 +1,9 @@
 
+let $button = $('#set-color');
+let brushBox = $('.brush');
+let input = $('input');
+$button.on('click', (event) => {
+	brushBox.css('background-color', input.val())
+	console.log('there was a click');
+	event.preventDefault();
+})
