42b1f9f694180560c953d935918a4587d2c9c17f
Noemi Romero <nlll02@yahoo.com>
2018-03-23 16:41:19 -0500

    Set Color

--- pixart.js
+++ pixart.js
@@ -1,1 +1,23 @@
+console.log('friday')
+
+//whent the button is clicked run the func getQuilt
+function changeC(event){
+	const inp = $('#color-field').val()
+	$('.brush').css({
+		'background-color': inp
+	})
+	event.preventDefault();
+}
+	$('#set-color').on('click', changeC)
+
+
+
+
+
+
+
+
+
+
+
 
