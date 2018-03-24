b13a6957bd7fb6fb95dc9c01584592c6ea96f7af
Irwin <irgranados16@gmail.com>
2018-03-24 13:59:51 -0500

    completed commit 1

--- pixart.js
+++ pixart.js
@@ -1,1 +1,11 @@
+const button = $("#set-color");
 
+function setColor() {
+	const color = $('#color-field').val();
+	let brush = $('.brush')
+	brush.css({
+		'background-color': color
+	})
+	event.preventDefault();
+}
+button.on("click", setColor);
