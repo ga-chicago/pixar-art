7cb830ca88532ad6fa40a633fa502ffd883bd35e
Irwin <irgranados16@gmail.com>
2018-03-24 17:29:28 -0500

    finished commited 6 and bonus

--- pixart.js
+++ pixart.js
@@ -1,16 +1,26 @@
 const button = $("#set-color");
+$brushColor = ''
+$count = 0
+
+function setSwatch(event) {
+	let swatches = $('.swatch')
+	if ($count === swatches.length) { $count = 0 }
+	setColor(swatches.eq($count), event)
+	$count++
+}
 
 function setColor(element, event) {
-	const color = $('#color-field').val();
 	element.css({
-		'background-color': color
+		'background-color': $brushColor
 	})
 	event.preventDefault();
 }
 
 button.click((event) => {
 	let brush = $('.brush')
+	$brushColor = $('#color-field').val();
 	setColor(brush, event)
+	setSwatch(event)
 });
 
 ////// Commit 2 is already working when pressing the enter key from inside the input field ////////////////
@@ -24,6 +34,18 @@ for (let i = 0; i < 8000; i++) {
 $('.square').mouseover((event) => {
 	let square = $(event.currentTarget)
 	setColor(square, event)
+})
+
+for (let i = 0; i < 3; i++) {
+	const swatch = $('<div>')
+	swatch.addClass('swatch')
+	$('.controls').append(swatch)
+}
+
+$('.swatch').click((event) => {
+	$brushColor = $(event.currentTarget).css('background-color')
+	let brush = $('.brush')
+	setColor(brush, event)
 })
 
 
