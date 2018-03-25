ad7535097848dda1b68f5d58de0b103402eedf40
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-03-24 19:36:32 -0500

    Got Brush to change color based on user input

--- pixart.js
+++ pixart.js
@@ -1,1 +1,39 @@
 
+
+
+document.getElementById('set-color').addEventListener('click',(event)=>{
+	
+
+	const color = document.getElementById('color-field').value
+	
+	document.getElementsByClassName('brush')[0].style.backgroundColor = color;
+	
+	
+
+	document.getElementById('canvas').addEventListener('mouseover',(event)=>{
+
+
+		
+
+
+
+	})
+
+
+
+
+
+	// console.log(this);
+
+	// console.log(event);
+
+	// console.log(event.target);
+
+	// console.log(event.currentTarget);
+	event.preventDefault();
+})
+
+
+// $('#set-color').on('click',()=>{
+// 	alert('Hello');
+// })
